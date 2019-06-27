c84803777a7ccda6fd643a50b275d118d7a5a347
anand kumar tiwari <aktiwari@nextgen.com>
2019-06-27 09:26:31 +0530

    second example for cicd

---
+++ src/main/java/com/anand/awscodebuilddemo/AwsCodebuildDemoApplication.java
@@ -0,0 +1,13 @@
+package com.anand.awscodebuilddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodebuildDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodebuildDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anand/awscodebuilddemo/AwsCodebuildDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anand.awscodebuilddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsCodebuildDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
